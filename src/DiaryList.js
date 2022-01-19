88210a439afb1007307f56aaed825ce5a53e833e
Nomadzz <82443328+Camelllia@users.noreply.github.com>
2022-01-19 16:56:52 +0900

    List Rendering

---
+++ src/DiaryList.js
@@ -0,0 +1,22 @@
+import DiaryItem from "./DiaryItem.js";
+
+const DiaryList = ({diaryList}) => {
+    return (
+    <div className="DiaryList">
+        <h2>일기 리스트</h2>
+        <h4>{diaryList.length}개의 일기가 있습니다.</h4>
+        <div>
+            {/* map으로 각 배열 순회 */}
+            {diaryList.map((it)=>(
+            <DiaryItem key={it.id} {...it}/>
+            ))}
+        </div>
+    </div>)
+}
+
+//예외처리
+DiaryList.defaultProps = {
+    diaryList: [],
+};
+
+export default DiaryList;

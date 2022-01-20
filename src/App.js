112b751708656e9aeb4b1c35e909ac61dbd414a6
Nomadzz <82443328+Camelllia@users.noreply.github.com>
2022-01-20 21:41:10 +0900

    Divide Components

--- src/App.js
+++ src/App.js
@@ -1,89 +1,114 @@
-import { useEffect, useRef, useState, useCallback, useMemo } from 'react/cjs/react.development';
-import './App.css';
-import DiaryEditor from './DiaryEditor';
-import DiaryList from './DiaryList';
+import React, {
+  useCallback,
+  useEffect,
+  useMemo,
+  useReducer,
+  useRef
+} from "react";
+import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
+import "./App.css";
 
-function App() {
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "INIT": {
+      return action.data;
+    }
+    case "CREATE": {
+      const created_date = new Date().getTime();
+      const newItem = {
+        ...action.data,
+        created_date
+      };
+      return [newItem, ...state];
+    }
+    case "REMOVE": {
+      return state.filter((it) => it.id !== action.targetId);
+    }
+    case "EDIT": {
+      return state.map((it) =>
+        it.id === action.targetId
+          ? {
+              ...it,
+              content: action.newContent
+            }
+          : it
+      );
+    }
+    default:
+      return state;
+  }
+};
 
-  const [data, setData] = useState([]);
-
+const App = () => {
+  const [data, dispatch] = useReducer(reducer, []);
   const dataId = useRef(0);
 
-  const getData = async() => {
+  //API 호출
+  const getData = async () => {
     const res = await fetch(
-      'https://jsonplaceholder.typicode.com/comments'
-      ).then((res)=>res.json());
-    
+      "https://jsonplaceholder.typicode.com/comments"
+    ).then((res) => res.json());
 
-      const initData = res.slice(0,20).map((it)=>{
-        return {
-          author : it.email,
-          content : it.body,
-          emotion : Math.floor(Math.random() * 5) + 1,
-          created_date : new Date().getTime(),
-          id : dataId.current++
-        };
-      });
+    const initData = res.slice(0, 20).map((it) => {
+      return {
+        author: it.email,
+        content: it.body,
+        emotion: Math.floor(Math.random() * 5) + 1,
+        created_date: new Date().getTime(),
+        id: dataId.current++
+      };
+    });
 
-      setData(initData);
+    dispatch({ type: "INIT", data: initData });
   };
 
-  useEffect(()=>{
+  useEffect(() => {
     getData();
-  },[])
+  }, []);
 
-  //데이터 생성 함수
+  //생성 함수
   const onCreate = useCallback((author, content, emotion) => {
-    const created_date = new Date().getTime();
-    const newItem = {
-        author,
-        content,
-        emotion,
-        created_date,
-        id : dataId.current
-    }
+    dispatch({
+      type: "CREATE",
+      data: { author, content, emotion, id: dataId.current }
+    });
     dataId.current += 1;
-    setData((data)=>[newItem, ...data]);
-  },
+  }, []);
 
-  []
-);
-
-  //데이터 삭제 함수
+  //삭제 함수
   const onRemove = useCallback((targetId) => {
-      setData(data.filter((it) => it.id !== targetId));
+    dispatch({ type: "REMOVE", targetId });
   }, []);
 
-  //데이터 수정 함수
+  //수정 함수
   const onEdit = useCallback((targetId, newContent) => {
-    //데이터가 바뀌었다면 컨텐츠를 수정해주고 아니라면 그대로 가져감
-    setData((data) =>
-      data.map((it)=>
-        it.id === targetId ? {...it, content:newContent} : it
-      )
-    );
+    dispatch({
+      type: "EDIT",
+      targetId,
+      newContent
+    });
   }, []);
 
-  //useMemo로 재연산 방지 >> 리렌더링 비용 감소(리팩토링)
-  const getDiaryAnalysis = useMemo(() => {
+  const memoizedDiaryAnalysis = useMemo(() => {
     const goodCount = data.filter((it) => it.emotion >= 3).length;
     const badCount = data.length - goodCount;
-    const goodRatio = (goodCount/data.length) * 100;
-    return{goodCount, badCount, goodRatio};
+    const goodRatio = (goodCount / data.length) * 100.0;
+    return { goodCount, badCount, goodRatio };
   }, [data.length]);
 
-  const {goodCount, badCount, goodRatio} = getDiaryAnalysis;
+  const { goodCount, badCount, goodRatio } = memoizedDiaryAnalysis;
 
   return (
     <div className="App">
-      <DiaryEditor onCreate={onCreate}/>
+      <DiaryEditor onCreate={onCreate} />
       <div>전체 일기 : {data.length}</div>
       <div>기분 좋은 일기 개수 : {goodCount}</div>
       <div>기분 나쁜 일기 개수 : {badCount}</div>
       <div>기분 좋은 일기 비율 : {goodRatio}%</div>
-      <DiaryList onRemove={onRemove} onEdit={onEdit} diaryList={data}/>
+      <DiaryList diaryList={data} onRemove={onRemove} onEdit={onEdit} />
     </div>
   );
-}
+};
 
 export default App;
